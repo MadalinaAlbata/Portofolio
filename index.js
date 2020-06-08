917c91c5952d74b68deb85ccb294ecfa4136546c
MadalinaAlbata <madalina_ioana55@yahoo.com>
2020-06-08 14:01:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+$(document).ready(function(e) {
+
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    console.log("sunt aici");
+    console.log(width);
+
+    toggle_onclick($win, $navbar, width);
+
+    //resize  event
+    $win.resize(function() {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function(e) {
+        $navbar.toggleClass('.toggle-left');
+    })
+});
+
+
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 768) {
+        $navbar.css({
+            left: "-200px" //`-$(width)px`
+        });
+    } else
+        $navbar.css({
+            left: "0px"
+        });
+
+}
+
+var typed = new Typed('#typed', {
+    strings: [
+        "Junior Web Developer",
+        "Passionate about coding"
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2', {
+    strings: [
+        "Junior Web Developer",
+        "Passionate about coding"
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
